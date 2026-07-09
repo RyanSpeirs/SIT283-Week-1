055701373891b11f9f6f5e4aa782539aaddff1e0
RyanSpeirs <61259710+RyanSpeirs@users.noreply.github.com>
2026-07-09 13:33:09 +1000

    Initial Commit

---
+++ Assets/Scripts/MakeBounceable.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class MakeBounceable : MonoBehaviour
+{
+    public GameObject bounceablePrefab;
+
+    public int numberOfBounceables = 10;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        CreateBounceables();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private async void CreateBounceables()
+    {
+        for (int i = 0; i < numberOfBounceables; i++)
+        {
+            GameObject g = Instantiate(bounceablePrefab);
+            await Awaitable.WaitForSecondsAsync(1);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision detected with " + collision.gameObject.name);
+
+        float x = Random.Range (0.2f, 0.8f);
+        float y = Random.Range(0.2f, 0.8f);
+        float z = Random.Range(0.2f, 0.8f);
+
+
+        collision.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(x,y,z) * 10, ForceMode.Impulse);
+    }
+
+}
